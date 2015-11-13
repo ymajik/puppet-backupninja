8d9fb8450118e3f15a99e9fc3f5bbc17fafec74c
Baptiste Grenier <bgrenier@gnubila.fr>
2015-11-13 10:33:01 +0100

    backupninja::server: drop copyright/autorship.

--- manifests/server.pp
+++ manifests/server.pp
@@ -21,15 +21,6 @@
 #  include '::puppet-backupninja::server'
 #
 # Configuration is done using Hiera.
-#
-# === Authors
-#
-# Baptiste Grenier <bgrenier@gnubila.fr>
-#
-# === Copyright
-#
-# Copyright 2015 gnúbila
-#
 class backupninja::server (
   $backupdir = '/backup',
   $backupserver_tag = $::fqdn,
